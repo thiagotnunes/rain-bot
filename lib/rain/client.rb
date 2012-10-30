03496e7fad7645ea08b57ae7ec7d8606b0c5e53e
Thiago Nunes <thiagotnunes@gmail.com>
2012-10-30 17:39:00 -0200

    Adding a torrent functionality.

--- lib/rain/client.rb
+++ lib/rain/client.rb
@@ -1,4 +1,11 @@
 module Rain
   class Client
+    def initialize(transmission)
+      @transmission = transmission
+    end
+
+    def add(url)
+      @transmission.add(url)
+    end
   end
 end
--- spec/lib/rain/client_spec.rb
+++ spec/lib/rain/client_spec.rb
@@ -1,6 +1,25 @@
 require 'spec_helper'
 
+class TransmissionAdapter
+end
+
 describe Rain::Client do
-  it 'should pass' do
+  let(:transmissionAdapter) { TransmissionAdapter.new }
+  subject { Rain::Client.new(transmissionAdapter) }
+
+  context 'adding a new torrent' do
+    it 'adds successfully' do
+      transmissionAdapter.stub(:add).with("url").and_return(true)
+
+      subject.add("url").should be_true
+    end
+
+    it 'adds unsuccessfully' do
+      transmissionAdapter.stub(:add).with("url").and_raise(IndexError.new)
+
+      expect {
+        subject.add("url")
+      }.to raise_error(IndexError)
+    end
   end
 end
